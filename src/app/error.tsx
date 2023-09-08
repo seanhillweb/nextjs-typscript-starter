2ec8d5c9146764cf80e60dfc09374bfa4cb526e9
seanhillweb <seanhillweb@gmail.com>
2023-09-08 11:53:06 -0500

    format: change file type to tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -10,7 +10,13 @@
 
 import { useEffect } from "react";
 
-export default function Error({ error, reset }) {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
